88406f291e65a00d84e34a5210c47f625a6da394
luoqi <50949439+logeexpluoqi@users.noreply.github.com>
2025-03-25 03:39:14 +0000

    fix(qbutton): fixed debounce logic bug

--- qbutton.c
+++ qbutton.c
@@ -2,7 +2,7 @@
  * @ Author: luoqi
  * @ Create Time: 2024-03-29 17:20
  * @ Modified by: luoqi
- * @ Modified time: 2025-02-17 20:58
+ * @ Modified time: 2025-03-25 11:26
  * @ Description:
  */
 
@@ -72,17 +72,21 @@ int qbutton_exec(QButton *button)
     int err = 0;
     int keyval = button->button_read();
 
-    // Check if the button is pressed and not active
-    if((keyval == button->press_keyval) && !button->isactive) {
-        if(button->debounce++ > button->debounce_tick) {
+    if(keyval == button->press_keyval) {
+        if(button->debounce < button->debounce_tick) {
+            button->debounce++;
+        }
+        if(button->debounce >= button->debounce_tick && !button->isactive) {
             button->state = QBUTTON_ACTION_PRESS_DOWN;
             button->isactive = 1;
-            button->debounce = 0;
+            button->debounce = 0; 
         }
     } else {
-        button->debounce = 0;
+        button->debounce = 0; 
+        button->isactive = 0; 
     }
 
+    // 状态机处理
     switch(button->state) {
     case QBUTTON_ACTION_NONE:
         button->isactive = 0;
@@ -97,8 +101,6 @@ int qbutton_exec(QButton *button)
                 button->state = QBUTTON_ACTION_PRESS_LONG_HOLD;
             }
         } else {
-            // If the long press threshold is not reached, consider it a normal release and enter multi-click wait state;
-            // If it is close to the long press threshold, there may be reading fluctuations, continue to maintain the state to trigger long press later
             if(button->ticks < button->long_tick) {
                 button->state = QBUTTON_ACTION_WAIT_MULTICLICK;
             }
@@ -114,39 +116,34 @@ int qbutton_exec(QButton *button)
         break;
 
     case QBUTTON_ACTION_WAIT_MULTICLICK:
-        // Accumulate timeout count during waiting period
         button->click_timeout++;
         if(keyval == button->press_keyval) {
-            // New button press indicates continuous click behavior, accumulate click count
             button->repeat++;
             button->state = QBUTTON_ACTION_PRESS_DOWN;
             button->click_timeout = 0;
             button->isactive = 1;
         } else if(button->click_timeout > button->short_tick) {
-            // Timeout, no new click, determine based on repeat value
             if(button->repeat == 0) {
-                // Single press, trigger single click event
                 if(button->callback[QBUTTON_ACTION_SINGLE_CLICK]) {
                     QBUTTON_EVENT_CALLBACK(QBUTTON_ACTION_SINGLE_CLICK, keyval, err);
                 } else {
                     QBUTTON_EVENT_CALLBACK(QBUTTON_ACTION_PRESS_UP, keyval, err);
                 }
             } else if(button->repeat == 1) {
-                // Double press, trigger double click event (fallback to single click if not registered)
-                if(button->callback[QBUTTON_ACTION_DOUBLE_CLICK])
+                if(button->callback[QBUTTON_ACTION_DOUBLE_CLICK]) {
                     QBUTTON_EVENT_CALLBACK(QBUTTON_ACTION_DOUBLE_CLICK, keyval, err);
-                else
+                } else {
                     QBUTTON_EVENT_CALLBACK(QBUTTON_ACTION_SINGLE_CLICK, keyval, err);
+                }
             } else if(button->repeat == 2) {
-                // Triple press, trigger triple click, then double click, finally single click
-                if(button->callback[QBUTTON_ACTION_TRIPLE_CLICK])
+                if(button->callback[QBUTTON_ACTION_TRIPLE_CLICK]) {
                     QBUTTON_EVENT_CALLBACK(QBUTTON_ACTION_TRIPLE_CLICK, keyval, err);
-                else if(button->callback[QBUTTON_ACTION_DOUBLE_CLICK])
+                } else if(button->callback[QBUTTON_ACTION_DOUBLE_CLICK]) {
                     QBUTTON_EVENT_CALLBACK(QBUTTON_ACTION_DOUBLE_CLICK, keyval, err);
-                else
+                } else {
                     QBUTTON_EVENT_CALLBACK(QBUTTON_ACTION_SINGLE_CLICK, keyval, err);
+                }
             } else {
-                // More button presses trigger continuous press event
                 QBUTTON_EVENT_CALLBACK(QBUTTON_ACTION_PRESS_REPEAT, keyval, err);
             }
             button->state = QBUTTON_ACTION_NONE;
@@ -155,6 +152,7 @@ int qbutton_exec(QButton *button)
             button->click_timeout = 0;
         }
         break;
+
     default:
         return -1;
     }
