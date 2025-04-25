ac0d72d7d74ccbd37db0b307072a5bfc720f6cad
luoqi <50949439+logeexpluoqi@users.noreply.github.com>
2025-04-25 03:14:27 +0000

    fix(qbutton): fixed qbutton double click and multi-click bug

--- qbutton.c
+++ qbutton.c
@@ -2,32 +2,32 @@
  * @ Author: luoqi
  * @ Create Time: 2024-03-29 17:20
  * @ Modified by: luoqi
- * @ Modified time: 2025-03-25 11:26
+ * @ Modified time: 2025-04-25 11:00
  * @ Description:
  */
 
 #include "qbutton.h"
 
-#define QBUTTON_EVENT_CALLBACK(action, keyval, local_err)   \
-    do {                                                    \
-        if (button->callback[action] != 0) {                \
-            int ret = button->callback[action](keyval);     \
-            local_err = ret;                                \
-        }                                                   \
+#define QBUTTON_EVENT_CALLBACK(action, kv, local_err)   \
+    do {                                                \
+        if (button->callback[action] != 0) {            \
+            int ret = button->callback[action](kv);     \
+            local_err = ret;                            \
+        }                                               \
     } while (0)
 
-int qbutton_init(QButton *button, QButtonPressDownKeyVal keyval, uint8_t debounce_tick, uint16_t long_tick, uint8_t short_tick, int (*button_read)(void))
+int qbutton_init(QButton *button, QButtonPressDownKeyVal kv, uint8_t debounce, uint16_t long_tick, uint8_t short_tick, int (*kv_read)(void))
 {
-    if(!button || !button_read) {
+    if(!button || !kv_read) {
         return -1;
     }
     button->isactive = 0;
-    button->debounce = 0;
+    button->dticks = 0;
     button->ticks = 0;
     button->repeat = 0;
-    button->press_keyval = keyval;
-    button->button_read = button_read;
-    button->debounce_tick = debounce_tick;
+    button->press_kv = kv;
+    button->kv_read = kv_read;
+    button->debounce = debounce;
     button->long_tick = long_tick;
     button->short_tick = short_tick;
     button->click_timeout = 0;
@@ -38,7 +38,7 @@ int qbutton_init(QButton *button, QButtonPressDownKeyVal keyval, uint8_t debounc
     return 0;
 }
 
-int qbutton_events_attach(QButton *button, QButtonAction action, int (*callback)(int))
+int qbutton_events_attach(QButton *button, QButtonAction action, int (*callback)(int kv))
 {
     if(!button || !callback) {
         return -1;
@@ -70,23 +70,22 @@ int qbutton_exec(QButton *button)
         return -1;
     }
     int err = 0;
-    int keyval = button->button_read();
+    int kv = button->kv_read();
 
-    if(keyval == button->press_keyval) {
-        if(button->debounce < button->debounce_tick) {
-            button->debounce++;
-        }
-        if(button->debounce >= button->debounce_tick && !button->isactive) {
+    if(kv == button->press_kv) {
+        if(button->dticks < button->debounce) {
+            button->dticks++;
+        } else if(!button->isactive) {
             button->state = QBUTTON_ACTION_PRESS_DOWN;
             button->isactive = 1;
-            button->debounce = 0; 
-        }
+            button->dticks = 0;
+        } 
+
     } else {
-        button->debounce = 0; 
-        button->isactive = 0; 
+        button->dticks = 0;
+        button->isactive = 0;
     }
 
-    // 状态机处理
     switch(button->state) {
     case QBUTTON_ACTION_NONE:
         button->isactive = 0;
@@ -94,10 +93,10 @@ int qbutton_exec(QButton *button)
         break;
 
     case QBUTTON_ACTION_PRESS_DOWN:
-        if(keyval == button->press_keyval) {
-            QBUTTON_EVENT_CALLBACK(QBUTTON_ACTION_PRESS_DOWN, keyval, err);
+        if(kv == button->press_kv) {
+            QBUTTON_EVENT_CALLBACK(QBUTTON_ACTION_PRESS_DOWN, kv, err);
             if(button->ticks++ > button->long_tick) {
-                QBUTTON_EVENT_CALLBACK(QBUTTON_ACTION_PRESS_LONG, keyval, err);
+                QBUTTON_EVENT_CALLBACK(QBUTTON_ACTION_PRESS_LONG, kv, err);
                 button->state = QBUTTON_ACTION_PRESS_LONG_HOLD;
             }
         } else {
@@ -108,8 +107,8 @@ int qbutton_exec(QButton *button)
         break;
 
     case QBUTTON_ACTION_PRESS_LONG_HOLD:
-        if(keyval != button->press_keyval) {
-            QBUTTON_EVENT_CALLBACK(QBUTTON_ACTION_PRESS_UP, keyval, err);
+        if(kv != button->press_kv) {
+            QBUTTON_EVENT_CALLBACK(QBUTTON_ACTION_PRESS_UP, kv, err);
             button->state = QBUTTON_ACTION_NONE;
             button->ticks = 0;
         }
@@ -117,7 +116,7 @@ int qbutton_exec(QButton *button)
 
     case QBUTTON_ACTION_WAIT_MULTICLICK:
         button->click_timeout++;
-        if(keyval == button->press_keyval) {
+        if(kv == button->press_kv) {
             button->repeat++;
             button->state = QBUTTON_ACTION_PRESS_DOWN;
             button->click_timeout = 0;
@@ -125,26 +124,26 @@ int qbutton_exec(QButton *button)
         } else if(button->click_timeout > button->short_tick) {
             if(button->repeat == 0) {
                 if(button->callback[QBUTTON_ACTION_SINGLE_CLICK]) {
-                    QBUTTON_EVENT_CALLBACK(QBUTTON_ACTION_SINGLE_CLICK, keyval, err);
+                    QBUTTON_EVENT_CALLBACK(QBUTTON_ACTION_SINGLE_CLICK, kv, err);
                 } else {
-                    QBUTTON_EVENT_CALLBACK(QBUTTON_ACTION_PRESS_UP, keyval, err);
+                    QBUTTON_EVENT_CALLBACK(QBUTTON_ACTION_PRESS_UP, kv, err);
                 }
             } else if(button->repeat == 1) {
                 if(button->callback[QBUTTON_ACTION_DOUBLE_CLICK]) {
-                    QBUTTON_EVENT_CALLBACK(QBUTTON_ACTION_DOUBLE_CLICK, keyval, err);
+                    QBUTTON_EVENT_CALLBACK(QBUTTON_ACTION_DOUBLE_CLICK, kv, err);
                 } else {
-                    QBUTTON_EVENT_CALLBACK(QBUTTON_ACTION_SINGLE_CLICK, keyval, err);
+                    QBUTTON_EVENT_CALLBACK(QBUTTON_ACTION_SINGLE_CLICK, kv, err);
                 }
             } else if(button->repeat == 2) {
                 if(button->callback[QBUTTON_ACTION_TRIPLE_CLICK]) {
-                    QBUTTON_EVENT_CALLBACK(QBUTTON_ACTION_TRIPLE_CLICK, keyval, err);
+                    QBUTTON_EVENT_CALLBACK(QBUTTON_ACTION_TRIPLE_CLICK, kv, err);
                 } else if(button->callback[QBUTTON_ACTION_DOUBLE_CLICK]) {
-                    QBUTTON_EVENT_CALLBACK(QBUTTON_ACTION_DOUBLE_CLICK, keyval, err);
+                    QBUTTON_EVENT_CALLBACK(QBUTTON_ACTION_DOUBLE_CLICK, kv, err);
                 } else {
-                    QBUTTON_EVENT_CALLBACK(QBUTTON_ACTION_SINGLE_CLICK, keyval, err);
+                    QBUTTON_EVENT_CALLBACK(QBUTTON_ACTION_SINGLE_CLICK, kv, err);
                 }
             } else {
-                QBUTTON_EVENT_CALLBACK(QBUTTON_ACTION_PRESS_REPEAT, keyval, err);
+                QBUTTON_EVENT_CALLBACK(QBUTTON_ACTION_PRESS_REPEAT, kv, err);
             }
             button->state = QBUTTON_ACTION_NONE;
             button->ticks = 0;
--- qbutton.h
+++ qbutton.h
@@ -2,7 +2,7 @@
  * @ Author: luoqi
  * @ Create Time: 2024-03-29 17:20
  * @ Modified by: luoqi
- * @ Modified time: 2025-02-14 16:11
+ * @ Modified time: 2025-04-25 11:00
  * @ Description:
  */
 
@@ -26,36 +26,76 @@ typedef enum {
     QBUTTON_ACTION_WAIT_MULTICLICK,
 } QButtonAction;
 
+/**
+ * @brief QButtonPressDownKeyVal defines the key value states for button press down.
+ */
 typedef enum {
     QBUTTON_PRESS_DOWN_KEYVAL_LOW = 0x00,
     QBUTTON_PRESS_DOWN_KEYVAL_HIGH = 0x01
 } QButtonPressDownKeyVal;
 
+/**
+ * @brief QButtonEvent is a function pointer type for button event callbacks.
+ * @param kv The key value associated with the event.
+ * @return An integer indicating the result of the callback.
+ */
 typedef int (*QButtonEvent)(int);
+
+/**
+ * @brief QButton represents the structure for a button with its properties and event callbacks.
+ */
 typedef struct {
-    uint8_t   press_keyval;
+    uint8_t   press_kv;
     uint16_t  ticks;
     uint8_t   debounce;
     uint8_t   state;
     uint8_t   isactive : 1;
     uint8_t   islongpress : 1;
     uint8_t   repeat : 6;
-    uint8_t   debounce_tick;
+    uint8_t   dticks;
     uint16_t  long_tick;        // long press time threshold
     uint8_t   short_tick;       // repeat press time threshold
     uint8_t   click_timeout;    // multi-click click timeout
 
-    int (*button_read)(void);
+    int (*kv_read)(void);
 
     QButtonEvent callback[7];
 } QButton;
 
-int qbutton_init(QButton *button, QButtonPressDownKeyVal keyval, uint8_t debounce_tick, uint16_t long_tick, uint8_t short_tick, int (*button_read)(void));
+/**
+ * @brief Initializes a QButton instance.
+ * @param button Pointer to the QButton instance.
+ * @param kv Initial key value state.
+ * @param debounce Debounce time in ticks.
+ * @param long_tick Long press time threshold in ticks.
+ * @param short_tick Repeat press time threshold in ticks.
+ * @param kv_read Function pointer to read the key value.
+ * @return 0 on success, non-zero on failure.
+ */
+int qbutton_init(QButton *button, QButtonPressDownKeyVal kv, uint8_t debounce, uint16_t long_tick, uint8_t short_tick, int (*kv_read)(void));
 
-int qbutton_events_attach(QButton *button, QButtonAction action, int (*callback)(int keyval));
+/**
+ * @brief Attaches a callback function to a specific button action.
+ * @param button Pointer to the QButton instance.
+ * @param action The button action to attach the callback to.
+ * @param callback Function pointer to the callback.
+ * @return 0 on success, non-zero on failure.
+ */
+int qbutton_events_attach(QButton *button, QButtonAction action, int (*callback)(int kv));
 
+/**
+ * @brief Detaches a callback function from a specific button action.
+ * @param button Pointer to the QButton instance.
+ * @param action The button action to detach the callback from.
+ * @return 0 on success, non-zero on failure.
+ */
 int qbutton_events_detach(QButton *button, QButtonAction action);
  
+/**
+ * @brief Executes the button logic, processing its state and triggering callbacks.
+ * @param button Pointer to the QButton instance.
+ * @return 0 on success, non-zero on failure.
+ */
 int qbutton_exec(QButton *button);
 
 #endif
